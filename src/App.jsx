02030065cee313246b32e76844f5a21d1d645a09
SunilRudraKumar <sunilrudrakumar@gmail.com>
2024-10-12 17:59:57 -0400

    Send Transaction

--- src/App.jsx
+++ src/App.jsx
@@ -1,38 +1,47 @@
-import React, { FC, useMemo } from 'react';
-import { ConnectionProvider, WalletProvider } from '@solana/wallet-adapter-react';
-import { WalletAdapterNetwork } from '@solana/wallet-adapter-base';
-import { UnsafeBurnerWalletAdapter } from '@solana/wallet-adapter-wallets';
+import React, { FC, useMemo } from "react";
 import {
-    WalletModalProvider,
-    WalletDisconnectButton,
-    WalletMultiButton
-} from '@solana/wallet-adapter-react-ui';
-import { clusterApiUrl } from '@solana/web3.js';
+  ConnectionProvider,
+  WalletProvider,
+} from "@solana/wallet-adapter-react";
+import { WalletAdapterNetwork } from "@solana/wallet-adapter-base";
+import { UnsafeBurnerWalletAdapter } from "@solana/wallet-adapter-wallets";
+import {
+  WalletModalProvider,
+  WalletDisconnectButton,
+  WalletMultiButton,
+} from "@solana/wallet-adapter-react-ui";
+import { clusterApiUrl } from "@solana/web3.js";
 
 // Default styles that can be overridden by your app
-import '@solana/wallet-adapter-react-ui/styles.css';
-import { RequestAirdrop } from './RequestAirdrop';
-import { ShowBalance } from './ShowBalance';
-import { SendToken } from './SendToken';
-
+import "@solana/wallet-adapter-react-ui/styles.css";
+import { RequestAirdrop } from "./RequestAirdrop";
+import { ShowBalance } from "./ShowBalance";
+import { SendToken } from "./SendToken";
+import { SignMessage } from "./SignTranscaion";
 
 function App() {
- 
   return (
     <ConnectionProvider endpoint={"https://api.devnet.solana.com/"}>
-    <WalletProvider wallets={[]} autoConnect>
+      <WalletProvider wallets={[]} autoConnect>
         <WalletModalProvider>
-          <div style={{width: "100vw",display:"flex",justifyContent:"center"}}>
+          <div
+            style={{
+              width: "100vw",
+              display: "flex",
+              justifyContent: "center",
+            }}
+          >
             <WalletMultiButton />
             <WalletDisconnectButton />
             {/* <RequestAirdrop />
             <ShowBalance/> */}
             <SendToken />
-            </div>
+            <SignMessage />
+          </div>
         </WalletModalProvider>
-    </WalletProvider>
-</ConnectionProvider>
+      </WalletProvider>
+    </ConnectionProvider>
   );
 }
 
-export default App
+export default App;
